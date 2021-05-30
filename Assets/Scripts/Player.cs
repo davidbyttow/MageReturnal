0d6ba56150dfb370f05b054ac3bd73f972c1625c
David Byttow <david.byttow@gmail.com>
2021-05-30 19:05:51 -0400

    simple projectiles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public float maxSpeed = 20;
+	public Projectile projectilePrefab;
 
 	private Rigidbody2D rigidBody;
 	private Vector2 moveInput = Vector2.zero;
 
-	void Start() {
+	void Awake() {
 		rigidBody = GetComponent<Rigidbody2D>();
 	}
 
@@ -18,9 +19,28 @@ public class Player : MonoBehaviour {
 		var moveY = Input.GetAxisRaw("Vertical");
 		moveInput = new Vector2(moveX, moveY);
 
+		if (Input.GetButtonDown("Fire1")) {
+			FireProjectile(aimDirection);
+		}
 	}
 
 	private void FixedUpdate() {
 		rigidBody.velocity = new Vector2(moveInput.x * maxSpeed, moveInput.y * maxSpeed);
+	}
+
+	private Vector2 aimDirection {
+		get {
+			var screenPos = Camera.main.WorldToScreenPoint(transform.position);
+			var cursorPos = Input.mousePosition;
+			var dir = (cursorPos - screenPos).normalized;
+			var dir2d = new Vector2(dir.x, dir.y);
+			return dir2d;
+		}
+	}
+
+	private void FireProjectile(Vector2 dir) {
+		Projectile proj = Instantiate(projectilePrefab, transform);
+		Physics2D.IgnoreCollision(GetComponent<Collider2D>(), proj.GetComponent<Collider2D>());
+		proj.SetVelocity(rigidBody.velocity + dir * 400);
 	}
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	private Rigidbody2D rigidBody;
+
+	void Awake() {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	public void SetVelocity(Vector2 velocity) {
+		rigidBody.AddForce(velocity);
+	}
+
+	void Update() {
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D target) {
+		Debug.Log("collision enter");
+		Destroy(gameObject);
+	}
+}
