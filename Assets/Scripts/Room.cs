cb6de40c07f99408daf4270b4b9c9fc9b1ebabbd
David Byttow <david.byttow@gmail.com>
2021-05-31 12:27:40 -0400

    refactors room traversal

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,19 +1,91 @@
 ﻿using UnityEngine;
 
 
+public enum RoomSide {
+	Top,
+	Right,
+	Bottom,
+	Left,
+}
+
+public static class RoomSides {
+	public static RoomSide Opposite(RoomSide side) {
+		switch (side) {
+			case RoomSide.Top: return RoomSide.Bottom;
+			case RoomSide.Right: return RoomSide.Left;
+			case RoomSide.Left: return RoomSide.Right;
+			case RoomSide.Bottom: return RoomSide.Top;
+		}
+		Debug.AssertFormat(false, "Unknown side {0}", side);
+		return RoomSide.Right;
+	}
+
+	public static Vector2Int Dir(RoomSide side) {
+		switch (side) {
+			case RoomSide.Top: return new Vector2Int(0, 1);
+			case RoomSide.Right: return new Vector2Int(1, 0);
+			case RoomSide.Left: return new Vector2Int(-1, 0);
+			case RoomSide.Bottom: return new Vector2Int(0, -1);
+		}
+		Debug.AssertFormat(false, "Unknown side {0}", side);
+		return Vector2Int.zero;
+	}
+}
+
 public class Room : MonoBehaviour {
 
 	public int width;
 	public int height;
-	public Door leftDoor;
-	public Door rightDoor;
+	public Door[] doors;
 
-	void Start() {
+	internal Vector2Int pos;
+	internal RoomController controller;
 
+	void Awake() {
+		foreach (var door in doors) {
+			door.room = this;
+			door.gameObject.SetActive(false);
+		}
 	}
 
-	void Update() {
+	public void ConnectRooms(RoomSide side, Room other) {
+		var srcDoor = GetDoor(side);
+		var dstDoor = other.GetDoor(RoomSides.Opposite(side));
+		srcDoor.gameObject.SetActive(true);
+		dstDoor.gameObject.SetActive(true);
+	}
 
+	public void ExitDoor(RoomSide side) {
+		controller.MoveToRoom(this, side);
+	}
+
+	public void EnterRoom(Player player, RoomSide side) {
+		var doorOffset = 1.5f;
+		var door = GetDoor(side);
+		var dir = RoomSides.Dir(side);
+		var offset = new Vector3(
+			-dir.x * doorOffset,
+			-dir.y * doorOffset,
+			0
+		);
+		player.transform.position = door.transform.position + offset;
+	}
+
+	Door FindDoor(RoomSide side) {
+		foreach (var d in doors) {
+			if (d.side == side) {
+				return d;
+			}
+		}
+		return null;
+	}
+
+	Door GetDoor(RoomSide side) {
+		var door = FindDoor(side);
+		if (!door) {
+			Debug.LogWarningFormat("Could not find door for side {0}", side);
+		}
+		return door;
 	}
 
 	private void OnDrawGizmos() {
--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -1,39 +1,90 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RoomController : MonoBehaviour {
 
+	public int gridSize = 6;
+	public float roomPadding = 2;
+
 	public Room roomPrefab;
 	private Room startRoom;
+	private Player player;
+
+	private Dictionary<Vector2Int, Room> rooms = new Dictionary<Vector2Int, Room>();
 
 	void Awake() {
 	}
 
 	void Start() {
-		startRoom = Instantiate(roomPrefab);
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+		int x = gridSize / 2;
+		int y = gridSize / 2;
+		Debug.Log($"Start room at {x},{y}");
+		startRoom = CreateRoom(new Vector2Int(x, y));
 		RoomCamera.inst.currentRoom = startRoom;
-		AddRoom(startRoom, startRoom.rightDoor);
+
+		CreateRoom(new Vector2Int(x + 1, y));
+		CreateRoom(new Vector2Int(x + 2, y));
+		CreateRoom(new Vector2Int(x, y + 1));
+		ConnectRooms();
 	}
 
 	void Update() {
 
 	}
 
-	void AddRoom(Room src, Door srcDoor) {
-		var padding = 2;
-		switch (srcDoor.side) {
-			case Door.Side.Right:
-				var room = Instantiate(roomPrefab);
-				room.transform.position = new Vector2(src.transform.position.x + src.width * 0.5f + padding + room.width * 0.5f, 0);
-				room.leftDoor.otherSide = srcDoor;
-				srcDoor.otherSide = room.leftDoor;
-				break;
-			default:
-				Debug.LogFormat("unknown side {0}", srcDoor.side);
-				return;
+	void ConnectRooms() {
+		foreach (var r in rooms) {
+			var pos = r.Key;
+			if (pos.x == 0 || pos.y == 0 || pos.x + 1 >= gridSize || pos.y + 1 >= gridSize) {
+				continue;
+			}
+			var room = r.Value;
+			foreach (var side in Enum.GetValues(typeof(RoomSide))) {
+				ConnectRoomIfPresent(room, (RoomSide)side);
+			}
 		}
 	}
 
-	void CreateRoom(Room prefab, int x, int y) {
+	void ConnectRoomIfPresent(Room room, RoomSide side) {
+		var dir = RoomSides.Dir(side);
+		var otherPos = room.pos + dir;
+		var other = FindRoom(otherPos);
+		if (other) {
+			var offset = new Vector3(
+				dir.x * (room.width * 0.5f + other.width * 0.5f + roomPadding),
+				dir.y * (room.height * 0.5f + other.height * 0.5f + roomPadding),
+				0
+			);
+			other.transform.position = room.transform.position + offset;
+			room.ConnectRooms(side, other);
+		}
+	}
+
+	Room CreateRoom(Vector2Int pos) {
+		var room = Instantiate(roomPrefab);
+		room.controller = this;
+		room.pos = pos;
+		rooms.Add(pos, room);
+		return room;
+	}
+
+	Room FindRoom(Vector2Int pos) {
+		if (!rooms.ContainsKey(pos)) {
+			return null;
+		}
+		return rooms[pos];
+	}
+
+	public void MoveToRoom(Room from, RoomSide side) {
+		var dir = RoomSides.Dir(side);
+		var dstPos = from.pos + dir;
+		var otherRoom = FindRoom(dstPos);
+		var otherSide = RoomSides.Opposite(side);
+		otherRoom.EnterRoom(player, otherSide);
+		RoomCamera.inst.currentRoom = otherRoom;
 	}
 }
